20d9e28b4c0d7426522b50b413bc90ce574ae60a
11praptigupta <69359222+11praptigupta@users.noreply.github.com>
2020-09-23 20:43:39 +0530

    Add files via upload

---
+++ precache-manifest.3c157f15f047b398960f1259fcce8b81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5bef13195972bdc16f3f76a080e469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4391616ce3f69f5845f7",
+    "url": "/static/js/2.43b9fa22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43b9fa22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9109cd6b23e57f32e412",
+    "url": "/static/js/main.56e35107.chunk.js"
+  },
+  {
+    "revision": "ffe148ba9c987666fc58",
+    "url": "/static/js/runtime-main.451dc41f.js"
+  }
+]);
